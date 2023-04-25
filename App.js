d2681b291ee4119c9cb32c0b316329058d9c5110
gagangani13 <127178242+gagangani13@users.noreply.github.com>
2023-04-25 10:16:27 +0530

    Fetch Movie on Page load

--- App.js
+++ App.js
@@ -1,31 +1,68 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import MovieList from "./components/MovieList";
 import "./App.css";
 
 function App() {
-  const[movies,setMovies]=useState([])
-  async function fetchMovies(){
-    const response= await fetch('https://swapi.dev/api/films');
-    const data=await response.json();
-    const transform=data.results.map((item)=>{
-      return{
-        id:item.episode_id,
-        title:item.title,
-        openingText:item.opening_crawl,
-        releaseDate:item.release_date
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(()=>{
+    console.log("use effect called");
+    fetchMovies()
+  },[])
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        fetchMovies()
+      }, 5000);
+    }
+    return(()=>{clearTimeout()})
+  }, [error]);
+
+  async function fetchMovies() {
+    setLoading(true);
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("Something went wrong");
       }
-    })
-    setMovies(transform)
+      const data = await response.json();
+      const transform = data.results.map((item) => {
+        setLoading(false);
+        return {
+          id: item.episode_id,
+          title: item.title,
+          openingText: item.opening_crawl,
+          releaseDate: item.release_date,
+        };
+      });
+      setMovies(transform);
+    } catch (err) {
+        setError(true)
+      
+    }
+  }
+  function cancelOperation() {
+    setError(false);
+    setLoading(false);
   }
 
   return (
     <React.Fragment>
+      {console.log('rendering')}
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MovieList movies={movies} />
+        {!loading && <MovieList movies={movies} />}
+        {loading && !error&& <p>LOADING...</p>}
+        {loading && error&& (
+          <>
+            <p>Something went wrong...Retrying</p>
+            <button onClick={cancelOperation}>Cancel</button>
+          </>
+        )}
       </section>
     </React.Fragment>
   );
